c403fbb42bc37a45dbb02f11299b45606912e83c
wcy123 <wcy123@gmail.com>
2015-05-25 10:07:26 +0800

    enable delete trailing space before saving buffers.

--- my.elisp/wcy-c-mode-hook.el
+++ my.elisp/wcy-c-mode-hook.el
@@ -1,6 +1,9 @@
 ;;;###autoload
 (defun wcy-c-mode-hook ()
   (setq c-basic-offset 4)
+  ;; prevent extern "C" { indenting
+  (c-set-offset 'inextern-lang 0)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace t t)
   (set (make-local-variable 'parens-require-spaces) nil))
 
 
