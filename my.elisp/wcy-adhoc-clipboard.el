4b355bb974d50e46632b75c055553d386f904e63
Wang Chunye <wcy123@gmail.com>
2015-08-24 15:00:50 +0800

    make it portable

--- my.elisp/wcy-adhoc-clipboard.el
+++ my.elisp/wcy-adhoc-clipboard.el
@@ -42,7 +42,7 @@
 ;; it returns nil the second time.  This is so that a single
 ;; selection won't be added to the kill ring over and over.
 ;;;###autoload
-(defun wcy-adhoc-clipboard-paste-function ()
+(defun wcy-adhoc-clipboard-get ()
   (let* ((content (wcy-adhoc-clipboard-content))
          (current-md5 (md5 content)))
     (if (string= wcy-adhoc-clipboard-paste-md5 current-md5)
@@ -50,33 +50,56 @@
       (setq wcy-adhoc-clipboard-paste-md5 current-md5)
       content)))
 ;;;###autoload
-(defun wcy-adhoc-clipboard-cut-function (text &optional push)
+(defun wcy-adhoc-clipboard-save (text &optional push)
   (wcy-adhoc-clipboard-save-content text))
 
 (require 'simple)
 (defvar wcy-adhoc-clipboard-old-interprogram-functions
   (cons interprogram-paste-function interprogram-cut-function))
+(defvar wcy-adhoc-orig-interprogram-paste-function
+  interprogram-paste-function)
+(defvar wcy-adhoc-orig-interprogram-cut-function
+  interprogram-cut-function)
+
 ;;;###autoload
 (defun wcy-adhoc-clipboard-enable ()
   (interactive)
+  (setq wcy-adhoc-orig-interprogram-paste-function
+        interprogram-paste-function
+        wcy-adhoc-orig-interprogram-cut-function
+        interprogram-cut-function)
   (setq interprogram-paste-function
 	#'(lambda (&rest args)
-	    (let ((x (apply 'x-cut-buffer-or-selection-value args)))
-	      (if x
-		  (progn
-		    (wcy-adhoc-clipboard-cut-function x))
-		(apply 'wcy-adhoc-clipboard-paste-function args)))))
+	    (let ((x (apply wcy-adhoc-orig-interprogram-paste-function args)))
+	      (if x x
+		(apply 'wcy-adhoc-clipboard-get args)))))
   (setq interprogram-cut-function
 	#'(lambda (&rest args)
-	    (apply 'x-select-text args)
-	    (apply 'wcy-adhoc-clipboard-cut-function args))))
+	    (apply 'wcy-adhoc-clipboard-save args)
+	    (apply wcy-adhoc-orig-interprogram-cut-function args))))
 
 (defun wcy-adhoc-clipboard-disable ()
   (interactive)
   (setq interprogram-paste-function
-	(car wcy-adhoc-clipboard-old-interprogram-functions)
+	wcy-adhoc-orig-interprogram-paste-function
 	interprogram-cut-function
-	(cdr wcy-adhoc-clipboard-old-interprogram-functions)))
+	wcy-adhoc-orig-interprogram-cut-function))
+
+;; (cond
+;;  ((string= system-type "darwin")
+;;   (defun wcy-get-clipboard ()
+;;     (shell-command-to-string "pbpaste"))
+;;   (defun wcy-set-clipboard (text)
+;;     (let ((process-connection-type nil))
+;;       (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;         (process-send-string proc text)
+;;         (process-send-eof proc)))))
+;;  (t
+;;   (defun wcy-get-clipboard ()
+
+;;   (defun wcy-set-clipboard (text)
+;;     (apply 'x-select-text text))
+
 
 (provide 'wcy-adhoc-clipboard)
 ;; Local Variables:
