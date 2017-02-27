ba5e947f568ad9df54d591ca1a7eba97e2940f7b
Wang Chunye <wcy123@gmail.com>
2017-02-27 16:42:09 +0800

    yes

--- my.elisp/wcy-init-my-package.el
+++ my.elisp/wcy-init-my-package.el
@@ -5,35 +5,38 @@
   (when (not (file-exists-p (expand-file-name "my.package" my-emacs-home)))
     (make-directory (expand-file-name "my.package" my-emacs-home)))
   (dolist (var
-	   '(("ace-jump-mode" "https://raw.githubusercontent.com/winterTTr/ace-jump-mode/master/ace-jump-mode.el"
-	      "ace-jump-mode.el")
-	     ("session" "http://www.emacswiki.org/emacs/download/session.el"
-	      "session.el")
-	     ("markdown-mode" "http://jblevins.org/projects/markdown-mode/markdown-mode.el"
-	      "markdown-mode.el")
-	     ("xcscope" "https://raw.githubusercontent.com/dkogan/xcscope.el/master/xcscope.el"
-	      "xcscope.el"
-	      )
+           '(("ace-jump-mode" "https://raw.githubusercontent.com/winterTTr/ace-jump-mode/master/ace-jump-mode.el"
+              "ace-jump-mode.el")
+             ("session" "http://www.emacswiki.org/emacs/download/session.el"
+              "session.el")
+             ("markdown-mode" "http://jblevins.org/projects/markdown-mode/markdown-mode.el"
+              "markdown-mode.el")
+             ("xcscope" "https://raw.githubusercontent.com/dkogan/xcscope.el/master/xcscope.el"
+              "xcscope.el"
+              )
              ("color-theme"
               "http://download.savannah.gnu.org/releases/color-theme/color-theme.el.gz"
               "color-theme.el.gz")
              ("solarized"
               "https://raw.githubusercontent.com/altercation/solarized/master/emacs-colors-solarized/color-theme-solarized.el"
               "color-theme-solarized.el")
+             ("robotframework"
+              "https://raw.githubusercontent.com/sakari/robot-mode/master/robot-mode.el"
+              "robot-mode.el")
              ))
     (apply #'wcy-init-my-package-simple var)))
 (defun wcy-init-my-package-simple(name url filename)
   (if (not (locate-file name load-path '(".elc" ".el")))
       (with-temp-message (format "retrieving %s" url)
-	(sit-for 0)
-	(let* ((default-directory (expand-file-name "my.package" my-emacs-home))
-	       (full-name (expand-file-name filename))
-	       (ret (call-process "curl" nil nil nil
-				  "-o"
-				  full-name
-				  url)))
-	  (if (eq ret 0)
-	      (condition-case nil
+        (sit-for 0)
+        (let* ((default-directory (expand-file-name "my.package" my-emacs-home))
+               (full-name (expand-file-name filename))
+               (ret (call-process "curl" nil nil nil
+                                  "-o"
+                                  full-name
+                                  url)))
+          (if (eq ret 0)
+              (condition-case nil
                   (let* ((ext (file-name-extension full-name))
                          (full-name-2 (if (string= ext "gz")
                                           (file-name-sans-extension full-name)
