6778fc33a8a7fdba5254d56b3adb5c95f4d81220
Wang Chunye <wcy123@gmail.com>
2019-07-01 22:05:38 -0400

    add wcy-maybe-copyright-update

--- my.elisp/wcy-c-mode-hook.el
+++ my.elisp/wcy-c-mode-hook.el
@@ -1,6 +1,29 @@
 ;;;###autoload
 (defun wcy-c-mode-hook ()
-  ;;(setq c-basic-offset 4)
+  (setq c-basic-offset 2)
   ;; prevent extern "C" { indenting
   (c-set-offset 'inextern-lang 0)
-  (set (make-local-variable 'parens-require-spaces) nil))
+  (set (make-local-variable 'parens-require-spaces) nil)
+  (add-hook 'before-save-hook 'clang-format-buffer t t)
+  (add-hook 'before-save-hook 'wcy-maybe-copyright-update t t)
+  )
+
+(defun wcy-maybe-copyright-update ()
+  (require 'copyright)
+  (let ((license-file (wcy-search-file-in-parent-dir '("LICENSE") "." )))
+    (when license-file
+      (let ((license (with-temp-buffer (insert-file-contents license-file)
+                                       (buffer-string))))
+        (save-excursion
+          (unless (copyright-find-copyright)
+            (goto-char 1)
+            (insert "/*")
+            (setq license-begin (point))
+            (insert "\n")
+            (insert license)
+            (setq license-end (point))
+            (insert "\n")
+            (replace-string "\n" "\n-- " nil license-begin license-end nil)
+            (insert "*/\n")
+            (delete-trailing-whitespace)
+            ))))))
