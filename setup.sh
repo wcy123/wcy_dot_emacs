e4895ff3dc7b9053b90a2a6d1d5825ee40158ee8
Wang Chunye <wcy123@gmail.com>
2019-08-02 00:32:41 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## install my customized dev enviroment
+cd $HOME
+mkdir -p $HOME/d/working
+cd $HOME/d/working
+git clone https://github.com/wcy123/wcy_dot_emacs.git
+git clone https://github.com/wcy123/leader-key-mode.git
