427bae9c6a1fb5cc00d20dc65be6d10f602a1743
Wang Chunye <wcy123@gmail.com>
2019-08-02 00:37:36 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -e
 ## install my customized dev enviroment
 cd $HOME
 mkdir -p $HOME/d/working
 cd $HOME/d/working
-git clone https://github.com/wcy123/wcy_dot_emacs.git
-git clone https://github.com/wcy123/leader-key-mode.git
+[ -d wcy_dot_emacs ] || git clone https://github.com/wcy123/wcy_dot_emacs.git
+[ -d leader-key-mode ] ||  clone https://github.com/wcy123/leader-key-mode.git
 ln -sf $HOME/d/working/wcy_dot_emacs/dot.emacs $HOME/.emacs
 mkdir -p $HOME/.emacs.d
