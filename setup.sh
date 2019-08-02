ef617a4c9d17d902989aa6dd0e977304817d8c47
Wang Chunye <wcy123@gmail.com>
2019-08-02 00:36:21 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ mkdir -p $HOME/d/working
 cd $HOME/d/working
 git clone https://github.com/wcy123/wcy_dot_emacs.git
 git clone https://github.com/wcy123/leader-key-mode.git
+ln -sf $HOME/d/working/wcy_dot_emacs/dot.emacs $HOME/.emacs
+mkdir -p $HOME/.emacs.d
